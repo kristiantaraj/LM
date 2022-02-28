3914976205da21c48d2aa595cbe4d4a6fa650f28
Kt.codes <tarajkristian@gmail.com>
2022-02-28 18:15:24 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+    $(window).on('load scroll', function () {
+        var scrolled = $(this).scrollTop();
+        $('#title').css({
+            'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
+            'opacity': 1 - scrolled / 400 // fade out at 400px from top
+        });
+        $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
+    });
+    
+
+    // $(window).scroll(function(){
+    //     if ($(this).scrollTop() > 150) {
+    //        $('.navbar-main').addClass('color');
+    //     } else {
+    //        $('.navbar-main').removeClass('color');
+    //     }
+    // });
+
+    // $('.slick-slider').slick({
+    //   dots: true,
+    //   infinite: true,
+    //   speed: 700,
+    //   autoplay:true,
+    //   autoplaySpeed: 1000,
+    //   arrows:false,
+    //   slidesToShow: 5,
+    //   slidesToScroll: 1
+    // });
+
+     
+    setTimeout(function thisDo() {
+        $('.hide').removeClass('hide');
+        $('.main').addClass('fade-in');
+    }, 3000);
+
+
+    function TimedEvent() {
+        $('.loader').addClass('hide');
+      };
+      setTimeout(TimedEvent, 3000);
+
+(function($) { "use strict";
+
+$(function() {
+    var header = $(".start-style");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 10) {
+            header.removeClass('start-style').addClass("scroll-on");
+        } else {
+            header.removeClass("scroll-on").addClass('start-style');
+        }
+    });
+});		
+    
+//Animation
+
+$(document).ready(function() {
+    $('body.hero-anime').removeClass('hero-anime');
+});
+
+//Menu On Hover
+    
+$('body').on('mouseenter mouseleave','.nav-item',function(e){
+        if ($(window).width() > 750) {
+            var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+            setTimeout(function(){
+            _d[_d.is(':hover')?'addClass':'removeClass']('show');
+            },1);
+        }
+});	
+
+})(jQuery); 
+});
+
